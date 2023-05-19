5bf4d24802f68813084d5c5c6aa8a27b62a8528f
DanielWieder <danielwieder94@gmail.com>
2023-05-19 13:09:52 +0300

    log in protected routes

--- my-vacations/src/Utils/ProtectedRoutes.tsx
+++ my-vacations/src/Utils/ProtectedRoutes.tsx
@@ -3,10 +3,10 @@ import { Outlet, Navigate } from "react-router-dom";
 import { vacationlyStore } from "../Components/Redux/VacationlyStore";
 import { isLoggedIn } from "../Components/Redux/UserReducer";
 const PrivateRoutes = () => {
-  let user = vacationlyStore.getState().users.isLoggedIn;
-  console.log("PrivateRoutes isLoggedIn: ", user);
+  let userLogged = vacationlyStore.getState().users.isLoggedIn;
+  console.log("PrivateRoutes isLoggedIn: ", userLogged);
 
-  return isLoggedIn(user) ? <Outlet /> : <Navigate to="/login" />;
+  return userLogged ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
