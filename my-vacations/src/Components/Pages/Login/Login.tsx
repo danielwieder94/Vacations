b1771617763361eb4b924b17465738f1c9af7ed9
DanielWieder <danielwieder94@gmail.com>
2023-06-21 15:34:24 +0300

    checking api requests

--- my-vacations/src/Components/Pages/Login/Login.tsx
+++ my-vacations/src/Components/Pages/Login/Login.tsx
@@ -39,7 +39,7 @@ function Login(): JSX.Element {
   const onSubmit = async (data: FormData) => {
     try {
       const response = await axios.post(
-        process.env.USERS_API_ENDPOINT + "/login" ||
+        process.env.USERS_API_ENDPOINT ||
           "http://localhost:4000/api/v1/users/login",
         data
       );
